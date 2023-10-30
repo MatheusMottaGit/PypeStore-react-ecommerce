a235857333cfbdbb9a4c15fa07e8211aa82d317e
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-30 20:48:24 -0300

    feat: adding filter

--- src/app/men/page.tsx
+++ src/app/men/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { ArrowDown } from 'lucide-react'
+import { ArrowDown, SlidersHorizontal } from 'lucide-react'
 import React from 'react'
 
 const Men = () => {
@@ -9,7 +9,7 @@ const Men = () => {
     <main>
 
       {/* presentation */}
-      <div className="h-[60vh] bg-[url('assets/men.png')] bg-center bg-cover flex items-center justify-start px-44">
+      <div className="h-[40vh] bg-[url('assets/men.png')] bg-center bg-cover flex items-center justify-start px-44">
         <div className="flex flex-col gap-4">
           <h1 className="text-7xl font-bold">Male section available!</h1>
 
@@ -24,10 +24,23 @@ const Men = () => {
         </div>
       </div>
 
-      <div className="bg-zinc-100 flex items-center flex-col gap-16 py-10 px-32">
-        <h1 className='font-bold'>
-          Choose by categories
-        </h1>
+      <div className="bg-zinc-100 flex items-center flex-col gap-16 py-5 px-32">
+
+        {/* filter management */}
+        <div className='flex items-center justify-between w-full'>
+          <div className='flex space-x-2 items-center border-r border-black px-2'>
+            <SlidersHorizontal className='w-4 h-4'/>
+            <h2>Filters</h2>
+          </div>
+
+          <div className='flex'>
+            <span>
+              Results
+            </span>
+
+            
+          </div>
+        </div>
       </div>
     </main>
   )

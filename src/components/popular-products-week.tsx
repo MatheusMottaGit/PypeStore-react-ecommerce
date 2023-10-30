22e07dc3d765db2850f5befde52593130a6d1ac7
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-30 15:41:00 -0300

    feat: setting components

---
+++ src/components/popular-products-week.tsx
@@ -0,0 +1,25 @@
+import { api } from '@/lib/axios'
+import React from 'react'
+import ProductCard, { Product } from './product-card'
+
+const PopularProductsWeek = async () => {
+  const response = await api.get('/products')
+  const products: Product[] = response.data
+
+  return (
+    <div className='grid grid-cols-5 gap-8 place-items-center'>
+      {products.slice(0,10).map((prod) => {
+        return (
+          <ProductCard 
+            id={prod.id}
+            name={prod.name}
+            price={prod.price}
+            url={prod.url}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default PopularProductsWeek
---
+++ src/components/product-card.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Card, CardDescription, CardTitle } from './ui/card'
+import Image from 'next/image'
+
+export interface Product {
+  id: number
+  name: string
+  url: string
+  price: number
+}
+
+const ProductCard = ({ id, name, price, url }: Product) => {
+  return (
+    <Card key={id} className='rounded-sm text-center space-y-4'>
+      <Image 
+        src={url}
+        alt=''
+        width={200}
+        height={200}
+        className='h-56'
+      />
+
+      <CardTitle>
+        {name}
+      </CardTitle>
+      <CardDescription className='font-semibold'>
+        R$ {price}
+      </CardDescription>
+    </Card>
+  )
+}
+
+export default ProductCard

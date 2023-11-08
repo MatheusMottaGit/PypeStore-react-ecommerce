b6010259b236459ef202fcc7a8dbfd0e45564186
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-08 15:03:46 -0300

    feat: add shadcn

--- src/components/filtered-products.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useFilter } from "@/contexts/filter-context";
-import React, { useEffect } from "react";
-
-const FilteredProducts = () => {
-  return <div className="grid grid-cols-6 place-items-center"></div>;
-};
-
-export default FilteredProducts;

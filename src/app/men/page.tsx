42e2dedbdbf4a185771098745cac06f91efa64cd
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-08 15:04:00 -0300

    feat: setting layouts

--- src/app/men/page.tsx
+++ src/app/men/page.tsx
@@ -1,4 +1,3 @@
-import FilteredProducts from "@/components/filtered-products";
 import FilterSelect from "@/components/men/filter-select";
 import { Button } from "@/components/ui/button";
 import { ArrowDown, SlidersHorizontal } from "lucide-react";
@@ -7,7 +6,39 @@ import React from "react";
 const Men = () => {
   return (
     // men page
-    <></>
+    <main>
+      {/* presentation */}
+      <div className="h-[40vh] bg-[url('../assets/men.png')] bg-center bg-cover flex items-center justify-start px-44">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-7xl font-bold">Male section available!</h1>
+
+          <span className="w-2/4 text-zinc-900">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et ullam
+            dolorem ad, exercitationem doloribus nostrum voluptatum odit
+            doloremque! Debitis est iure nobis molestiae vel tempore quia quis
+            minima harum sunt?
+          </span>
+
+          <Button className="w-64 mt-12 bg-ground text-zinc-300 gap-1">
+            View products
+            <ArrowDown className="w-4 h-4" />
+          </Button>
+        </div>
+      </div>
+
+      {/* products section */}
+      <div className="bg-zinc-100 flex items-center flex-col gap-16 py-5 px-32">
+        {/* filter management bar*/}
+        <div className="flex items-center justify-between w-full">
+          <div className="flex space-x-2 items-center">
+            <SlidersHorizontal className="w-4 h-4" />
+            <h2 className="border-r px-2 border-r-ground">Filters</h2>
+          </div>
+
+          <FilterSelect />
+        </div>
+      </div>
+    </main>
   );
 };
 
--- src/components/menu-list.tsx
+++ src/components/menu-list.tsx
@@ -1,35 +1,60 @@
-import Link from 'next/link'
-import React from 'react'
-import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card'
+import Link from "next/link";
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "For men",
+    href: "/men",
+    description: "A section to show you our male products and variants.",
+  },
+  {
+    title: "For women",
+    href: "/women",
+    description: "A section to show you our female products and variants.",
+  },
+];
 
 const MenuList = () => {
   return (
-    <ul className='flex items-center gap-12 text-xs cursor-pointer'>
-      <Link href='/'>
-        Home
-      </Link>
+    <ul className="flex items-center gap-12 text-xs cursor-pointer">
+      <Link href="/">Home</Link>
 
-      <HoverCard>
-        <HoverCardTrigger>
-          Products
-        </HoverCardTrigger>
-        <HoverCardContent>
-          <div className='flex flex-col gap-6'>
-            <Link href='/men'>
-              Men
-            </Link>
-            <Link href='/women'>
-              Women
-            </Link>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-xs">
+              Products
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="flex gap-5 items-center w-[480px] p-3">
+                {components.map(({ title, description, href }) => {
+                  return (
+                    <a
+                      href={href}
+                      className="space-y-1 hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground transition-colors"
+                    >
+                      <div className="font-semibold text-base">{title}</div>
 
-      <Link href='/contact'>
-        Contact
-      </Link>
+                      <p>{description}</p>
+                    </a>
+                  );
+                })}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <Link href="/contact">Contact</Link>
     </ul>
-  )
-}
+  );
+};
 
-export default MenuList
+export default MenuList;

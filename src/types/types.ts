5d50a648b7a560a1e582834db93691d9552f9122
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-08 13:51:31 -0300

    feat: global types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,16 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface Product {
-  id: number
-  name: string
-  url: string
-  price: number
-  clothe?: string
+  id: number;
+  name: string;
+  url: string;
+  price: number;
+  clothe?: string;
 }
 
-export type Clothe = "t-shirt" | "pants" | "shoes" | "jacket" | "dress"
+export type Clothe = "t-shirt" | "pants" | "shoes" | "jacket" | "dress";
 
 export type SelectValues = {
-  id: string
-  value: string
-  title: string
-  clothe: Clothe
-} 
+  id: string;
+  value: string;
+  title: string;
+  clothe: Clothe;
+};

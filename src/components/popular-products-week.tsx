428cea2a83ee91eca264af229023f833c20cb37b
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-08 13:51:43 -0300

    feat: code improving

--- src/components/popular-products-week.tsx
+++ src/components/popular-products-week.tsx
@@ -1,28 +1,27 @@
-import { api } from '@/lib/axios'
-import React from 'react'
-import ProductCard from './product-card'
-import { Product } from '@/types/types'
+import { api } from "@/lib/axios";
+import React from "react";
+import ProductCard from "./product-card";
+import { Product } from "@/types/types";
 
 const PopularProductsWeek = async () => {
-
-  const response = await api.get('/products')
-  const products: Product[] = response.data
+  const response = await api.get("/products");
+  const products: Product[] = response.data;
 
   return (
-    <div className='grid grid-cols-5 gap-8 place-items-center'>
+    <div className="grid grid-cols-5 gap-8 place-items-center">
       {products.slice(0, 5).map((prod) => {
         return (
           <ProductCard
-            key={prod.id} 
+            key={prod.id}
             id={prod.id}
             name={prod.name}
             price={prod.price}
             url={prod.url}
           />
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default PopularProductsWeek
+export default PopularProductsWeek;

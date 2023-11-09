e6b43048c86d59264008276710cb572ad3374ef4
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-09 17:47:41 -0300

    feat: changing to filtered prods page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,17 @@
-import FilterSelect from "@/components/men/filter-select";
+import FilterCategorySelect from "@/components/filter-category-select";
+import FilterClotheSelect from "@/components/filter-clothe-select";
+import FilteredProducts from "@/components/filtered-products";
 import { Button } from "@/components/ui/button";
 import { ArrowDown, SlidersHorizontal } from "lucide-react";
 import React from "react";
 
-const Men = () => {
+const Products = () => {
   return (
-    // men page
     <main>
       {/* presentation */}
-      <div className="h-[40vh] bg-[url('../assets/men.png')] bg-center bg-cover flex items-center justify-start px-44">
+      <div className="bg-[url('../assets/men.png')] h-[40vh] bg-fixed bg-cover flex items-center justify-center px-44">
         <div className="flex flex-col gap-4">
-          <h1 className="text-7xl font-bold">Male section available!</h1>
+          <h1 className="text-7xl font-bold">Check our summer edition!</h1>
 
           <span className="w-2/4 text-zinc-900">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et ullam
@@ -20,7 +21,7 @@ const Men = () => {
           </span>
 
           <Button className="w-64 mt-12 bg-ground text-zinc-300 gap-1">
-            View products
+            View all
             <ArrowDown className="w-4 h-4" />
           </Button>
         </div>
@@ -35,11 +36,18 @@ const Men = () => {
             <h2 className="border-r px-2 border-r-ground">Filters</h2>
           </div>
 
-          <FilterSelect />
+          <div className="flex space-x-4">
+            <FilterCategorySelect />
+
+            <FilterClotheSelect />
+          </div>
         </div>
+
+        {/* filtered products */}
+        <FilteredProducts />
       </div>
     </main>
   );
 };
 
-export default Men;
+export default Products;

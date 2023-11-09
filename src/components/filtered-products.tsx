73c77e17630db83d38b9c11d6fb862fd2b9f5270
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-09 18:54:49 -0300

    feat: filtered products pages

--- src/components/filtered-products.tsx
+++ src/components/filtered-products.tsx
@@ -1,17 +1,8 @@
 "use client";
-import { useFetch } from "@/hooks/useFetch";
 import React from "react";
 
 const FilteredProducts = () => {
-  const { products } = useFetch();
-
-  return (
-    <div className="grid grid-cols-6">
-      {products?.map((prod) => {
-        return <p>{prod.price}</p>;
-      })}
-    </div>
-  );
+  return <></>;
 };
 
 export default FilteredProducts;

3d96445bc68dc6f70845fdf047630067c1d00424
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-09 17:49:20 -0300

    feat: query filter function

---
+++ src/helpers/database-filter.ts
@@ -0,0 +1,28 @@
+import { Category, Clothe } from "@/types/types";
+
+export function getSelectedCategory(category: Category) {
+  if (category === Category.WOMEN) return "women's";
+  if (category === Category.MEN) return "men's";
+
+  return "";
+}
+
+export function getSelectedClotheType(clothe: Clothe | string) {
+  if (clothe === "t-shirt") return "t-shirt";
+  if (clothe === "shoes") return "shoes";
+  if (clothe === "pants") return "pants";
+  if (clothe === "jacket") return "jacket";
+  if (clothe === "dress") return "dress";
+
+  return "";
+}
+
+export function amountQueries(category: Category, clothe: Clothe | string) {
+  const selectedCategory = getSelectedCategory(category); //male or female
+
+  const selectedClotheType = getSelectedClotheType(clothe);
+
+  return `
+    http://localhost:3333/products?clothe=${selectedClotheType}&category=${selectedCategory}
+  `;
+}

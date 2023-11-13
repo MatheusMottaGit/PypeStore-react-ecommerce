1f2cf955d3979e69badf06102585ed85b14868c6
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-13 12:15:55 -0300

    feat: setting filter helper

--- src/helpers/database-filter.ts
+++ src/helpers/database-filter.ts
@@ -1,28 +1,28 @@
-// import { Category, Clothe } from "@/types/types";
+import { Category, Clothe } from "@/types/types";
 
-// export function getSelectedCategory(category: Category) {
-//   if (category === Category.WOMEN) return "women's";
-//   if (category === Category.MEN) return "men's";
+export function getSelectedCategory(category: Category) {
+  if (category === Category.WOMEN) return "women's";
+  if (category === Category.MEN) return "men's";
 
-//   return "";
-// }
+  return "";
+}
 
-// export function getSelectedClotheType(clothe: Clothe | string) {
-//   if (clothe === "t-shirt") return "t-shirt";
-//   if (clothe === "shoes") return "shoes";
-//   if (clothe === "pants") return "pants";
-//   if (clothe === "jacket") return "jacket";
-//   if (clothe === "dress") return "dress";
+export function getSelectedClotheType(clothe: Clothe) {
+  if (clothe === Clothe["T-SHIRT"]) return "t-shirt";
+  if (clothe === Clothe.SHOES) return "shoes";
+  if (clothe === Clothe.PANTS) return "pants";
+  if (clothe === Clothe.JACKET) return "jacket";
+  if (clothe === Clothe.DRESS) return "dress";
 
-//   return "";
-// }
+  return "";
+}
 
-// export function amountQueries(category: Category, clothe: Clothe | string) {
-//   const selectedCategory = getSelectedCategory(category); //male or female
+export function amountQueries(category: Category, clothe: Clothe) {
+  const selectedCategory = getSelectedCategory(category); //male or female
 
-//   const selectedClotheType = getSelectedClotheType(clothe);
+  const selectedClotheType = getSelectedClotheType(clothe);
 
-//   return `
-//     http://localhost:3333/products?clothe=${selectedClotheType}&category=${selectedCategory}
-//   `;
-// }
+  return `
+    http://localhost:3333/products?clothe=${selectedClotheType}&category=${selectedCategory}
+  `;
+}

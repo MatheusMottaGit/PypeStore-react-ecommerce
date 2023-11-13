deef12042a852003c780ae1e60a86fdacbc59afb
Matheus Motta <matheusdomingues423@gmail.com>
2023-11-13 12:14:26 -0300

    feat: build filtered products

--- src/components/filtered-products.tsx
+++ src/components/filtered-products.tsx
@@ -1,8 +1,27 @@
 "use client";
+import { useFetch } from "@/hooks/useFetch";
 import React from "react";
+import ProductCard from "./product-card";
 
 const FilteredProducts = () => {
-  return <></>;
+  const { products, isLoading } = useFetch();
+
+  return (
+    <div className="grid grid-cols-6 gap-4 place-items-center">
+      {products?.map(({ id, name, price, url, overview }) => {
+        return (
+          <ProductCard
+            key={id}
+            id={id}
+            name={name}
+            price={price}
+            url={url}
+            overview={overview}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default FilteredProducts;
